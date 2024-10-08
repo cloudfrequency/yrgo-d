74719c6e983e635bb06eb92a931630d731f96f62
Mikael Foxell <mikael@foxell.se>
2024-10-08 13:20:27 +0200

    Kanske duger? Oklart

--- pwm_styr.py
+++ pwm_styr.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 import scipy.signal
-from gpiozero import PWMLED
+#from gpiozero import PWMLED
+#from gpiozero import LED
 
-fs = 50000  # Samplingsfrekvens i Hz
-t = np.linspace(0, 1, fs)  # 1 s 
+fs = 5000  # Samplingsfrekvens i Hz
+t = np.linspace(0, 0.02, fs)  # 1 s 
 
 f = 50  # Frekvens i Hz
 sinus = np.sin(2 * np.pi * f * t)
@@ -14,12 +15,15 @@ f_saw = 250 # Sawtooth Hz
 sawtooth = scipy.signal.sawtooth(2 * np.pi * f_saw * t)
 
 square_wave = []
-for i in range(50000):
+for i in range(len(sinus)):
     if sinus[i] > sawtooth[i]:
         square_wave.append(1)
     else:
         square_wave.append(0)
-    
+
+print(f'Length of Sinus: {len(sinus)}')
+print(f'Length of Sawtooth: {len(sawtooth)}')    
+print(f'Length of Square: {len(square_wave)}')
 
 def display_plots():
     #for i in range(len(square_wave)):
@@ -56,15 +60,15 @@ def display_plots():
     plt.legend()
     plt.grid(True)
     plt.show()
-
-pwm_pin = PWMLED(12, frequency=50)
-
+'''
+#pwm_pin = PWMLED(12, frequency=50)
+pwm_pin = LED(12)
 for j in range(20):
-    for i in range(50000):
+    for i in range(len(square_wave)):
         if square_wave[i] == 1:
-        pwm_pin.on()
-    else:
-        pwm_pin.off()
-
+            pwm_pin.on()
+        else:
+            pwm_pin.off()
+'''
 #Uncomment to display plots
-#display_plots()
+display_plots()
